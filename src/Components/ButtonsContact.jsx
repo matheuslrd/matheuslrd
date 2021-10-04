9ba1584617db2b860999b3ef7750c24a3ac9723a
Matheus Laurindo <matheus104521@gmail.com>
2021-10-04 14:37:20 -0300

    Reorganiza os botoes de contato

---
+++ src/Components/ButtonsContact.jsx
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+import { FaWhatsapp, FaLinkedin, FaGithub } from 'react-icons/fa';
+
+class ButtonsContact extends Component {
+  render() {
+    return (
+      <>
+        <button type="button" className="Btn-Contact Btn-Whatsapp">
+          <FaWhatsapp size="1.4em" /> 
+          <span className="whats">whatsapp</span>
+        </button>
+        <button type="button" className="Btn-Contact Btn-LinkedIn">
+          <FaLinkedin size="1.4em" /> 
+          <span className="whats">LinkedIn</span>
+        </button>
+        <button type="button" className="Btn-Contact Btn-GitHub">
+          <FaGithub size="1.4em" /> 
+          <span className="whats">GitHub</span>
+        </button>
+      </>
+    );
+  }
+}
+
+export default ButtonsContact;
