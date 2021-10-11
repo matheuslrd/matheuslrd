c1b914d6e0fc94a770770127c3d81dedd9de1b92
Matheus Laurindo <matheus104521@gmail.com>
2021-10-10 21:05:07 -0300

    Atualiza resumo sobre habilidades

--- src/Components/AbstractSkills.jsx
+++ src/Components/AbstractSkills.jsx
@@ -5,8 +5,10 @@ function AbstractSkills() {
     <section className="Abstract-Skills">
       <h2 className="Skills-Title"> Habilidades </h2>
       <p className="Abstract-Skills-Text">
-        Tenho conhecimentos em HTML5, CSS3, JavaScript, testes unitários com Assert,
-        testes automatizados com Jest e React Testing Library para React.
+        Conhecimentos em HTML5, CSS3, JavaScript, testes automatizados com
+        Jest e React Testing Library, além de aprimorar diáriamente minhas softskills.
+        <br />
+        <br />
         Atualmente aprimoro meus conhecimentos em React e suas bibliotecas como Redux.
       </p>
     </section>
