61d04caa3c105bb9d4da154555314f9fdbb72025
Matheus Laurindo <matheus104521@gmail.com>
2021-11-29 11:47:01 -0300

    Remove projetos genericos

--- src/data.js
+++ src/data.js
@@ -1,13 +1,6 @@
 const projects = [
   {
     id: 1,
-    title: 'Title Project',
-    description: 'Aqui estará escrito sobre o que é o projeto e qual sua finalidade.',
-    linkWebSite: '#',
-    linkGitHub: '#',
-  },
-  {
-    id: 2,
     title: 'Jogo Trivia',
     description: `Jogo de perguntas desenvolvido em equipe utilizando Metodologias Ágeis, React e Bootstrap 
     para a Trybe.`,
@@ -15,7 +8,7 @@ const projects = [
     linkGitHub: 'https://github.com/matheuslrd/projeto-trivia',
   },
   {
-    id: 3,
+    id: 2,
     title: 'Your Wallet',
     description: `Desenvolvido a fim de estudos para Trybe, para validar
     conhecimentos em React e Redux.`,
@@ -23,26 +16,20 @@ const projects = [
     linkGitHub: 'https://github.com/matheuslrd/yourwallet',
   },
   {
-    id: 4,
+    id: 3,
     title: 'Online Store',
-    description: `Desenvolvido em equipe utilizando Metodologias Ágeis SCRUM, React e Tailwind
+    description: `Desenvolvido em equipe utilizando Metodologia Ágil KANBAN, React e Tailwind
     para a Trybe.`,
     linkWebSite: 'https://frontend-online-store-lemon.vercel.app/',
     linkGitHub: 'https://github.com/matheuslrd/frontend-online-store',
   },
   {
-    id: 5,
-    title: 'Title Project',
-    description: 'Aqui estará escrito sobre o que é o projeto e qual sua finalidade.',
-    linkWebSite: '#',
-    linkGitHub: '#',
-  },
-  {
-    id: 6,
-    title: 'Title Project',
-    description: 'Aqui estará escrito sobre o que é o projeto e qual sua finalidade.',
-    linkWebSite: '#',
-    linkGitHub: '#',
+    id: 4,
+    title: 'Three Meals',
+    description: `Desenvolvido em equipe utilizando Metodologia Ágil KANBAN, React, React
+    Router, Hooks, Context Api para a Trybe.`,
+    linkWebSite: '',
+    linkGitHub: '',
   },
 ];
 
