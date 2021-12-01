51a7264ae1e2cffee0dd2312bb50b2238bf2966e
Matheus Laurindo <matheus104521@gmail.com>
2021-12-01 16:47:41 -0300

    Adiciona cound tunes nos projetos

--- src/data.js
+++ src/data.js
@@ -10,7 +10,7 @@ const projects = [
   {
     id: 2,
     title: 'Your Wallet',
-    description: `Desenvolvido a fim de estudos para Trybe, para validar
+    description: `Carteira digital desenvolvido a fim de estudos para Trybe, para validar
     conhecimentos em React e Redux.`,
     linkWebSite: 'https://matheuslrd.github.io/yourwallet/',
     linkGitHub: 'https://github.com/matheuslrd/yourwallet',
@@ -18,18 +18,17 @@ const projects = [
   {
     id: 3,
     title: 'Online Store',
-    description: `Desenvolvido em equipe utilizando Metodologia Ágil KANBAN, React e Tailwind
-    para a Trybe.`,
+    description: `Aplicativo de e-commerce desenvolvido em equipe utilizando Metodologia Ágil KANBAN,
+    React e Tailwind para a Trybe.`,
     linkWebSite: 'https://frontend-online-store-lemon.vercel.app/',
     linkGitHub: 'https://github.com/matheuslrd/frontend-online-store',
   },
   {
     id: 4,
-    title: 'Three Meals',
-    description: `Desenvolvido em equipe utilizando Metodologia Ágil KANBAN, React, React
-    Router, Hooks, Context Api para a Trybe.`,
-    linkWebSite: '',
-    linkGitHub: '',
+    title: 'Cound Tunes',
+    description: 'Aplicativo de músicas desenvolvido utilizando React e requisições a API.',
+    linkWebSite: 'https://cound-tunes.vercel.app/',
+    linkGitHub: 'https://github.com/matheuslrd/cound-tunes',
   },
 ];
 
